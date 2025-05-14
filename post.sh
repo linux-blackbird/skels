2c0017c19bee9316b2d9d230cf7e6c513138b272
almuhdilkarim <almuhdilkarim2@gmail.com>
2025-05-15 03:15:36 +0700

    testing variable at post install

--- post.sh
+++ post.sh
@@ -1,2 +1,16 @@
 #!/bin/bash
 
+source /install/userenv
+source /install/protcolenv
+
+
+echo $MAKEUSER
+echo $PROTOCOL
+echo $PROTOCOL
+echo $NETSIPV4
+echo $NETSGATE
+echo $NETSDNSS
+echo $USERPACK
+echo $TIMEZONE
+echo $HOSTNAME
+
