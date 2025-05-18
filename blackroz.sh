4f7f5fb2acb46652f36c8533e6d0586ba9750b42
almuhdilkarim <almuhdilkarim2@gmail.com>
2025-05-18 13:14:48 +0700

    update

--- blackroz.sh
+++ blackroz.sh
@@ -2,84 +2,61 @@
 
 ### ADMINISTRATOR
 
-cryptsetup luksFormat /dev/nvme0n1p2
 
-cryptsetup luksFormat /dev/nvme0n1p3
+yes | mkfs.vfat -F32 -S 4096 -n BOOT /dev/sdb1
 
-cryptsetup luksFormat /dev/nvme0n1p4
+cryptsetup luksFormat /dev/sdb2
 
-cryptsetup luksOpen /dev/nvme0n1p2 lvm_keys
+cryptsetup luksOpen /dev/sdb2 luks_keys
 
-yes | mkfs.ext4 -L KEYS /dev/mapper/lvm_keys 
-
-cryptsetup luksOpen /dev/nvme0n1p3 lvm_root
-
-cryptsetup luksOpen /dev/nvme0n1p4 lvm_data
+yes | mkfs.ext4 -L KEYS -b 4096 /dev/mapper/lvm_keys 
 
 
 ### TECHNICAL
 
-pvcreate /dev/mapper/lvm_root
+yes | mkfs.ext4 -L ROOT -b 4096 /dev/sdb3
 
-vgcreate proc /dev/mapper/lvm_root
+yes | mkfs.ext4 -L VARS -b 4096 /dev/sdb4
 
-yes | lvcreate -L 10G proc -n root
+yes | mkfs.ext4 -L VLOG -b 4096 /dev/sdb5
 
-yes | lvcreate -L 5G proc -n vars
+yes | mkfs.ext4 -L VAUD -b 4096 /dev/sdb6
 
-yes | lvcreate -L 1.5G proc -n vtmp
+yes | mkfs.ext4 -L VTMP -b 4096 /dev/sdb7
 
-yes | lvcreate -L 5G proc -n vlog
+yes | mkswap /dev/proc/sdb8
 
-yes | lvcreate -L 2.5G proc -n vaud
+yes | mkfs.ext4 -L HOME -b 4096 /dev/sdb9
 
-yes | lvcreate -l100%FREE proc -n swap
+yes | mkfs.ext4 -L WEB1 -b 4096 /dev/sdb10
 
-pvcreate /dev/mapper/lvm_data
+yes | mkfs.ext4 -L WEB2 -b 4096 /dev/sdb11
 
-vgcreate data /dev/mapper/lvm_data
 
-yes | lvcreate -L 5G data -n home
 
-yes | lvcreate -l100%FREE data -n host
+mount /dev/sdb3 /mnt/
 
-yes | mkfs.vfat -F32 -S 4096 -n BOOT /dev/nvme0n1p1
+mkdir /mnt/boot  && mount -o uid=0,gid=0,fmask=0077,dmask=0077 /dev/sdb1 /mnt/boot
 
-yes | mkfs.ext4 -b 4096 /dev/data/home
+mkdir /mnt/var && mount -o defaults,rw,nosuid,nodev,noexec,relatime /dev/sdb4 /mnt/var
 
-mkfs.xfs -fs size=4096 /dev/data/host
+mkdir /mnt/var/log && mount -o rw,nosuid,nodev,noexec,relatime /dev/sdb5 /mnt/var/log
 
-yes | mkfs.ext4 -b 4096 /dev/proc/root
+mkdir /mnt/var/log/audit && mount -o rw,nosuid,nodev,noexec,relatime /dev/sdb6 /mnt/var/log/audit
 
-yes | mkfs.ext4 -b 4096 /dev/proc/vars
+mkdir /mnt/var/tmp && mount -o rw,nosuid,nodev,noexec,relatime /dev/sdb7 /mnt/var/tmp
 
-yes | mkfs.ext4 -b 4096 /dev/proc/vtmp
+swapon /dev/sdb8
 
-yes | mkfs.ext4 -b 4096 /dev/proc/vlog
+mkdir /mnt/home && mount -o rw,nosuid,nodev,noexec,relatime /dev/sdb9  /mnt/home 
 
-yes | mkfs.ext4 -b 4096 /dev/proc/vaud
+mkdir /mnt/srv/ /mnt/srv/http /mnt/srv/http/public  && mount -o rw,nosuid,nodev,noexec,relatime /dev/sdb10 /mnt/srv/http/public
 
-yes | mkswap /dev/proc/swap
+mkdir /mnt/srv/http/intern  && mount -o rw,nosuid,nodev,noexec,relatime /dev/sdb11 /mnt/srv/http/intern
 
-mount /dev/proc/root /mnt/
 
-mkdir /mnt/boot && mount -o uid=0,gid=0,fmask=0077,dmask=0077 /dev/nvme0n1p1 /mnt/boot
 
-mkdir /mnt/var && mount -o defaults,rw,nosuid,nodev,noexec,relatime /dev/proc/vars /mnt/var
-
-mkdir /mnt/var/tmp && mount -o rw,nosuid,nodev,noexec,relatime /dev/proc/vtmp /mnt/var/tmp
-
-mkdir /mnt/var/log && mount -o rw,nosuid,nodev,noexec,relatime /dev/proc/vlog /mnt/var/log
-
-mkdir /mnt/var/log/audit && mount -o rw,nosuid,nodev,noexec,relatime /dev/proc/vaud /mnt/var/log/audit
-
-mkdir /mnt/home && mount -o rw,nosuid,nodev,noexec,relatime /dev/data/home /mnt/home 
-
-mkdir /mnt/var/lib /mnt/var/lib/libvirt /mnt/var/lib/libvirt/images && mount /dev/data/host /mnt/var/lib/libvirt/images 
-
-swapon /dev/proc/swap
-
-pacstrap /mnt/ linux-hardened linux-firmware mkinitcpio intel-ucode xfsprogs lvm2 base base-devel neovim git luksmeta clevis mkinitcpio-nfs-utils openssh polkit less firewalld tang apparmor libpwquality rsync qemu-base libvirt openbsd-netcat reflector nftables tuned tuned-ppd irqbalance
+pacstrap /mnt/ linux-hardened linux-firmware mkinitcpio amd-ucode base base-devel neovim git luksmeta clevis mkinitcpio-nfs-utils openssh polkit less firewalld tang apparmor libpwquality rsync reflector nftables tuned tuned-ppd irqbalance
 
 genfstab -U /mnt/ > /mnt/etc/fstab 
 
@@ -97,7 +74,7 @@ cp -fr conf/bbconfig/vhosted/* /mnt/
 
 arch-chroot /mnt
 
-echo blacksky > /etc/hostname
+echo blackroz > /etc/hostname
 
 ln -sf /usr/share/zoneinfo/Asia/Jakarta /etc/localtime
 
@@ -125,25 +102,19 @@ passwd lektor
 
 mkdir /opt/cockpit
 
-useradd -d /opt/cockpit nepster
+useradd -d /opt/cockpit h3x0r
 
-usermod -a -G wheel nepster
+usermod -a -G wheel h3x0r
 
-echo 'nepster ALL=(ALL:ALL) ALL' > /etc/sudoers.d/00_lektor
+echo 'h3x0r ALL=(ALL:ALL) ALL' > /etc/sudoers.d/00_lektor
 
-chown nepster:nepster /opt/cockpit
+chown h3x0r:h3x0r /opt/cockpit
 
-passwd nepster
+passwd h3x0r
 
 passwd -l root
 
-useradd -d /opt/var/lib/livirt/images joyboy
-
-setfacl -Rm u:joyboy:rw /var/lib/libvirt/images
-
-passwd joyboy
-
-su nepster
+su h3x0r
 
 git clone https://aur.archlinux.org/mkinitcpio-clevis-hook /tmp/clevis
 
@@ -157,10 +128,6 @@ makepkg -sric --dir /tmp/aide --noconfirm
 
 exit
 
-usermod -a -G libvirt joyboy
-
-usermod -a -G libvirt nepster
-
 
 ### TECHNICAL
 
@@ -169,13 +136,13 @@ systemctl enable systemd-networkd.socket
 
 systemctl enable systemd-resolved
 
-echo "cryptdevice=UUID=$(blkid -s UUID -o value /dev/nvme0n1p3):crypto root=/dev/proc/root" > /etc/cmdline.d/01-boot.conf
+echo "root=/dev/sdb3" > /etc/cmdline.d/01-boot.conf
 
 echo "data UUID=$(blkid -s UUID -o value /dev/nvme0n1p4) none" >> /etc/crypttab
 
 echo "intel_iommu=on i915.fastboot=1" >> /etc/cmdline.d/02-mods.conf
 
-mv /boot/intel-ucode.img /boot/vmlinuz-linux-hardened /boot/kernel
+mv /boot/amd-ucode.img /boot/vmlinuz-linux-hardened /boot/kernel
 
 rm /boot/initramfs-*
 
@@ -198,8 +165,6 @@ cp /etc/pacman.d/mirrorlist /etc/pacman.d/backupmirror
 systemctl enable tuned-ppd
 
 systemctl enable irqbalance.service
-
-systemctl enable libvirtd.socket
 
 chown root:root /etc/crontab && chmod og-rwx /etc/crontab
 
@@ -236,8 +201,6 @@ modprobe -r afs 2> /dev/null && rmmod afs 2> /dev/null
 
 modprobe -r fuse 2> /dev/null && rmmod fuse 2> /dev/null
 
-systemctl mask nfs-server.service
-
 modprobe -r dccp 2> /dev/null && rmmod dccp 2>/dev/null
 
 modprobe -r rds 2> /dev/null && rmmod rds 2> /dev/null
@@ -254,28 +217,8 @@ reboot
 
 ln -sf ../run/systemd/resolve/stub-resolv.conf /etc/resolv.conf
 
-firewall-cmd --zone=public --add-port=7500/tcp --permanent
-
-firewall-cmd --reload
-
-echo "ip=10.10.1.22::10.10.1.1:255.255.255.0::eth0:none nameserver=10.10.1.1" > /etc/cmdline.d/06-nets.conf
-
-clevis luks bind -d /dev/nvme0n1p3 tang '{"url":"http://10.10.1.2:7500"}'
-clevis luks bind -d /dev/nvme0n1p3 tang '{"url":"http://10.10.1.22:7500"}'
-clevis luks bind -d /dev/nvme0n1p4 tang '{"url":"http://10.10.1.2:7500"}'
-clevis luks bind -d /dev/nvme0n1p4 tang '{"url":"http://10.10.1.22:7500"}'
-
-clevis luks list -d /dev/nvme0n1p3 
-clevis luks list -d /dev/nvme0n1p4   
-
-
-systemctl enable clevis-luks-askpass.path
-
 pacman -S libpam-google-authenticator qrencode
 
 su lektor
 
 google-authenticator
-
-
-
